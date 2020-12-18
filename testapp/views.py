610f16a54bbb463c4e65f54c0ec82bd04492e110
Vandris <v.stepanov92@yandex.ru>
2020-12-19 01:32:09 +0700

    First view for MyModel

--- testapp/views.py
+++ testapp/views.py
@@ -1,3 +1,9 @@
 from django.shortcuts import render
-
+from models import MyModel
 # Create your views here.
+
+
+def index(request):
+    data = MyModel.objects.all()
+    context = {'data': data}
+    return render(request, 'index.html', context)
