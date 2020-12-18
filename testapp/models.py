f1f8cd273d29acfdce2bacc35dd9d17d488fa415
Vandris <v.stepanov92@yandex.ru>
2020-12-19 01:22:18 +0700

    add first model

--- testapp/models.py
+++ testapp/models.py
@@ -1,3 +1,11 @@
 from django.db import models
 
 # Create your models here.
+
+
+class MyModel(models.Model):
+    field1 = models.CharField(max_length=100)
+    field2 = models.CharField(max_length=100)
+
+    def __str__(self):
+        return f"<Model: {self.field1}, {self.field2}"
