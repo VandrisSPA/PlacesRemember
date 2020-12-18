9f9163b8729bd0095b8bef4583a1eeef9429c958
Vandris <v.stepanov92@yandex.ru>
2020-12-19 02:14:33 +0700

    adding second view with adding data

--- testapp/views.py
+++ testapp/views.py
@@ -1,5 +1,6 @@
 from django.shortcuts import render
 from .models import MyModel
+from .forms import MyModelForm
 # Create your views here.
 
 
@@ -7,3 +8,17 @@ def index(request):
     data = MyModel.objects.all()
     context = {'data': data}
     return render(request, 'index.html', context)
+
+
+def index1(request):
+    if request.method == "POST":
+        form = MyModelForm(request.POST)
+
+        if form.is_valid():
+            form.save()
+            context = {'data': MyModel.objects.all()}
+            return render(request, 'index.html', context)
+    else:
+        form = MyModelForm()
+
+    return render(request, 'add_form.html', {'form': form})
