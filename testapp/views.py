61e2110c4fd39874a950bcbae4e90ae716afcfe9
Vandris <v.stepanov92@yandex.ru>
2020-12-20 19:22:56 +0700

    Adapting templates for user field

--- testapp/views.py
+++ testapp/views.py
@@ -1,18 +1,26 @@
 from django.shortcuts import render
+from django.contrib.auth.decorators import login_required
 from .models import MyModel
 from .forms import MyModelForm
 # Create your views here.
 
 
+def login(request):
+    return render(request, 'login.html')
+
+
+@login_required
 def index(request):
-    data = MyModel.objects.all()
+    data = MyModel.objects.all().filter(author=request.user)
     context = {'data': data}
     return render(request, 'index.html', context)
 
 
+@login_required
 def index1(request):
     if request.method == "POST":
         form = MyModelForm(request.POST)
+        form.instance.author = request.user
 
         if form.is_valid():
             form.save()
--- testing/urls.py
+++ testing/urls.py
@@ -14,10 +14,15 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
-from django.urls import path
+from django.urls import path, include
+from django.contrib.auth import views as auth_views
 from testapp import views
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('index', views.index1, name='first_model')
+    path('', views.index, name='home'),
+    path("login/", views.login, name="login"),
+    path('add_data', views.index1, name='first_model'),
+    path("logout/", auth_views.LogoutView.as_view(), name="logout"),
+    path('social-auth/', include('social_django.urls', namespace="social")),
 ]
