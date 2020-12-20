987779f6ced5e45a7c4dbb81432f975f0331f793
Vandris <v.stepanov92@yandex.ru>
2020-12-20 19:57:35 +0700

    Adding 'Add data' button

--- testing/urls.py
+++ testing/urls.py
@@ -22,7 +22,7 @@ urlpatterns = [
     path('admin/', admin.site.urls),
     path('', views.index, name='home'),
     path("login/", views.login, name="login"),
-    path('add_data', views.index1, name='first_model'),
+    path('add_data/', views.index1, name='first_model'),
     path("logout/", auth_views.LogoutView.as_view(), name="logout"),
     path('social-auth/', include('social_django.urls', namespace="social")),
 ]
