ed7e8799ae29a3d7b6111da3fd86a5ff63f03dec
Vandris <v.stepanov92@yandex.ru>
2020-12-20 19:19:48 +0700

    Adding user to model

--- testapp/models.py
+++ testapp/models.py
@@ -1,9 +1,11 @@
 from django.db import models
 from django.contrib.gis.db import models as g_models
+from django.conf import settings
 # Create your models here.
 
 
 class MyModel(models.Model):
+    author = models.ForeignKey(settings.AUTH_USER_MODEL, on_delete=models.CASCADE)
     field1 = models.CharField(max_length=100)
     field2 = models.CharField(max_length=100)
     field3 = models.IntegerField(default=-1)
