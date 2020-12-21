b063cdf546efc3f7f43af16eeff82d9a17efee0a
Vandris <v.stepanov92@yandex.ru>
2020-12-22 01:41:56 +0700

    Preparing for heroku

--- testing/settings.py
+++ testing/settings.py
@@ -151,8 +151,3 @@ STATIC_URL = '/static/'
 
 STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
 STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
-
-# Extra places for collectstatic to find static files.
-STATICFILES_DIRS = (
-    os.path.join(BASE_DIR, 'static'),
-)
